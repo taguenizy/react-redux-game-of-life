597ad4636fbb2a6cd8580fe17830ea31aa3fbe41
João Costa <costa.j@ua.pt>
2020-01-04 14:50:52 +0000

    add lint

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    "extends": "airbnb",
+    "plugins": ["jest"],
+    "parser": "babel-eslint",
+    "env": {
+        "browser": true,
+        "jest/globals": true
+    }
+};
